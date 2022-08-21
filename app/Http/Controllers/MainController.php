855126720e1d232f5048afedb854b89433d84d79
Corentin-W <corentin@3gimmobilier.com>
2022-08-21 17:48:31 +0200

    initial commit

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class MainController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function mainPage()
+    {
+        return view('welcome');
+    }
+}
